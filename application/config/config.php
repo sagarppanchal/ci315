0d7908989c91fb8c7e41297677d4abc93fc8cc9d
Sagarppanchal <sagarppanchal.ce@gmail.com>
2019-06-16 01:22:46 +0530

    added porto theme

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://172.10.24.37/git/ci315/'; //change link
+$config['base_url'] = 'http://localhost/projects/3pglobals/ConnectDonor/ci315/'; //change link
 
 /*
 |--------------------------------------------------------------------------
